c85fa5938003367ce8abf9f302f3fb85d0541caf
Abdus Salam <mrabdussalam@meta.com>
2025-02-23 01:47:23 -0800

    style: refine input focus states with elegant transitions and inset ring

--- src/components/ui/form/input.tsx
+++ src/components/ui/form/input.tsx
@@ -14,14 +14,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
         className={cn(
           "flex h-10 w-full rounded-lg border border-gray-200 bg-white px-3.5 py-2.5 text-sm",
           "placeholder:text-gray-500",
-          "focus:outline-none focus:border-transparent focus:ring-2 focus:ring-blue-500",
+          "focus:outline-none focus:ring-[0.5px] focus:ring-inset focus:ring-blue-500 focus:border-blue-500",
           "disabled:cursor-not-allowed disabled:opacity-50",
-          "transition-all duration-200",
+          "transition-[border,box-shadow] duration-200 ease-[cubic-bezier(0.4,0,0.2,1)]",
           "[appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none",
+          "[&:-webkit-autofill]:!bg-white [&:-webkit-autofill]:!bg-clip-padding",
+          "[&:-webkit-autofill]:!text-gray-900 [&:-webkit-autofill]:!shadow-[0_0_0_30px_white_inset]",
           error && "border-red-500 focus:ring-red-500",
           className
         )}
         ref={ref}
+        autoComplete="off"
         {...props}
       />
     )
